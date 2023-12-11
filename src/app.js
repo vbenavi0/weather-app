36ce5c29ce805fd7b8ef1e72f56cfbf003aabb6c
vbenavi0 <victorvill.0988@gmail.com>
2023-12-11 18:29:15 -0500

    completed up to video 49

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+const publicPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views') //directory for hbs files
+const partialPath = path.join(__dirname, '../templates/partials') //directory for partial files
+
+app.set('view engine', 'hbs') //tells express to use our handlebar hbs files
+app.set('views', viewPath) //tells express where views folder is
+hbs.registerPartials(partialPath) //tells hbs where to find partials
+
+app.use(express.static(publicPath))
+
+app.get('', (req, res)=>{
+    res.render('index',{ //render index view template 
+        title: 'Weather App', 
+        name: 'Victor'
+    })
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about',{
+        title: 'About Me', 
+        name: 'Victor'
+    })
+})
+
+app.get('/help', (req, res)=>{
+    res.render('help',{
+        message: 'How can I help?',
+        title: 'Help', 
+        name: 'Victor'
+    })
+})
+
+app.get('/weather', (req, res)=>{
+    res.send({
+        Location: "Charlotte",
+        Forecast: 'Rainy'
+    })
+    res.end()
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is listening on port 3000.')
+})
